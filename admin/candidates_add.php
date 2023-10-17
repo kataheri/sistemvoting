de3eba347b91729c9fe456839e98da0b81825d9d
herisetyoaji <heriaji72@gmail.com>
2023-10-17 07:31:14 +0700

    update jpg

--- admin/candidates_add.php
+++ admin/candidates_add.php
@@ -7,18 +7,25 @@
 		$group = $_POST['group'];
 		$platform = $_POST['platform'];
 		$filename = $_FILES['photo']['name'];
-		if(!empty($filename)){
-			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);	
-		}
+		$file_extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-		$sql = "INSERT INTO candidates (position_id, group_id, fullname, photo, platform) VALUES ('$position', '$group', '$fullname','$filename', '$platform')";
-		if($conn->query($sql)){
-			$_SESSION['success'] = 'Candidate added successfully';
+		// Validasi ekstensi file
+		if(!empty($filename) && in_array($file_extension, array("jpg", "jpeg", "png"))){
+			// Pindahkan file jika ekstensi valid
+			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);
+
+			$sql = "INSERT INTO candidates (position_id, group_id, fullname, photo, platform) VALUES ('$position', '$group', '$fullname', '$filename', '$platform')";
+
+			if($conn->query($sql)){
+				$_SESSION['success'] = 'Candidate added successfully';
+			}
+			else{
+				$_SESSION['error'] = $conn->error;
+			}
 		}
 		else{
-			$_SESSION['error'] = $conn->error;
+			$_SESSION['error'] = 'File gambar harus memiliki ekstensi .jpg, .jpeg, atau .png';
 		}
-
 	}
 	else{
 		$_SESSION['error'] = 'Fill up add form first';
